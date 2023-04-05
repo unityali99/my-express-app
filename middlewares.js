0411a2928f58334a91a173b3e85fa071e7d2b99d
Ali Ahmadi <mrkiller1383@gmail.com>
2023-04-05 14:41:11 +0430

    Created the auth middleware and will put it in use. Fixed the bug that main route middleware also applied for other routes like /:id

--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,5 @@
 const { isValidObjectId } = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 function checkPost(req, res, next) {
   if (req.method !== "POST")
@@ -14,11 +15,24 @@ function checkIdRoute(req, res, next, id) {
 }
 
 function checkMainRoute(req, res, next) {
+  if (req.url !== "/") return next();
   if (req.method === "DELETE" || req.method === "PUT")
     return res.status(405).send("Method is not allowed");
   next();
 }
 
+function auth(req, res, next) {
+  const token = req.header("X-Auth-Token");
+  if (!token) return res.status(401).send("Access Denied. No token provided");
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+  } catch (err) {
+    res.status(422).send("Invalid token provided");
+  }
+}
+
 module.exports.checkPost = checkPost;
 module.exports.checkIdRoute = checkIdRoute;
 module.exports.checkMainRoute = checkMainRoute;
+module.exports.auth = auth;
